2c9d9575a741db6a3339ac7524746c5800d5f214
bsse1006 <43725423+bsse1006@users.noreply.github.com>
2019-04-19 04:26:27 +0600

    Add files via upload

--- clustering.cpp
+++ clustering.cpp
@@ -2,10 +2,12 @@
 
 using namespace std;
 
-vector <profile> :: iterator itrProfile;
+int meanSameMarker = 0;
 
-vector<point> profileToPointsConverter (vector<profile> profiles)
+vector<point> profilesToPointsConverter (vector<profile> profiles)
 {
+    vector <profile> :: iterator itrProfile;
+
     vector<point> points;
 
     for(itrProfile=profiles.begin();itrProfile!=profiles.end();itrProfile++)
@@ -13,7 +15,7 @@ vector<point> profileToPointsConverter (vector<profile> profiles)
         for(int i=0; i<(*itrProfile).numberOfSamples; i++)
         {
             point tempPoint;
-            
+
             tempPoint.id=(*itrProfile).id;
 
             tempPoint.percentageOfEmptyLines=(*itrProfile).percentageOfEmptyLines.back();
@@ -35,4 +37,149 @@ vector<point> profileToPointsConverter (vector<profile> profiles)
     }
 
     return points;
+}
+
+point meanPoint (vector<point> points)
+{
+    vector <point> :: iterator itr;
+
+    point temp;
+
+    temp.id=-1;
+
+    for(itr=points.begin();itr!=points.end();itr++)
+    {
+        temp.percentageOfWhiteSpace+=(*itr).percentageOfWhiteSpace;
+        temp.percentageOfEmptyLines+=(*itr).percentageOfEmptyLines;
+        temp.spacePerSignature+=(*itr).spacePerSignature;
+        temp.percentageOfProperIndentation+=(*itr).percentageOfProperIndentation;
+        temp.percentageOfSpaceBeforeEqual+=(*itr).percentageOfSpaceBeforeEqual;
+        temp.percentageOfSpaceAfterEqual+=(*itr).percentageOfSpaceAfterEqual;
+    }
+
+    if(points.size()!=0)
+    {
+        temp.percentageOfWhiteSpace=temp.percentageOfWhiteSpace/points.size();
+        temp.percentageOfEmptyLines=temp.percentageOfEmptyLines/points.size();
+        temp.spacePerSignature=temp.spacePerSignature/points.size();
+        temp.percentageOfProperIndentation=temp.percentageOfProperIndentation/points.size();
+        temp.percentageOfSpaceBeforeEqual=temp.percentageOfSpaceBeforeEqual/points.size();
+        temp.percentageOfSpaceAfterEqual=temp.percentageOfSpaceAfterEqual/points.size();
+    }
+
+    return temp;
+}
+
+bool isSamePoint (point a, point b)
+{
+    if(a.percentageOfEmptyLines==b.percentageOfEmptyLines
+       &&a.percentageOfProperIndentation==b.percentageOfProperIndentation
+       &&a.percentageOfSpaceAfterEqual==b.percentageOfSpaceAfterEqual
+       &&a.percentageOfSpaceBeforeEqual==b.percentageOfSpaceBeforeEqual
+       &&a.percentageOfWhiteSpace==b.percentageOfWhiteSpace
+       &&a.spacePerSignature==b.spacePerSignature)
+        return true;
+    else false;
+}
+
+vector <cluster> meanPoints (vector<cluster> clusters)
+{
+    meanSameMarker = 0;
+
+    vector<cluster> :: iterator itr;
+
+    vector <cluster> temp;
+
+    for(itr=clusters.begin();itr!=clusters.end();itr++)
+    {
+        cluster tempCluster;
+        tempCluster.mean=meanPoint((*itr).clusPoints);
+        if(isSamePoint(tempCluster.mean,(*itr).mean))
+            meanSameMarker++;
+
+        temp.push_back(tempCluster);
+    }
+
+    return temp;
+}
+
+double distanceBetweenPoints (point a, point b)
+{
+    return sqrt(pow(a.percentageOfEmptyLines-b.percentageOfEmptyLines,2)
+                +pow(a.percentageOfProperIndentation-b.percentageOfProperIndentation,2)
+                +pow(a.percentageOfSpaceAfterEqual-b.percentageOfSpaceAfterEqual,2)
+                +pow(a.percentageOfSpaceBeforeEqual-b.percentageOfSpaceBeforeEqual,2)
+                +pow(a.percentageOfWhiteSpace-b.percentageOfWhiteSpace,2)
+                +pow(a.spacePerSignature-b.spacePerSignature,2));
+}
+
+vector <cluster> clustering (vector<point> points, vector<cluster> clusters)
+{
+    vector<point>::iterator pointIterator;
+    vector<cluster>::iterator clusterIterator;
+
+    for(pointIterator=points.begin();pointIterator!=points.end();pointIterator++)
+    {
+        vector<cluster>::iterator tempIterator;
+        double minimum = distance(*pointIterator,(clusters.front()).mean);
+        tempIterator=clusters.begin();
+        for(clusterIterator=clusters.begin();clusterIterator!=clusters.end();clusterIterator++)
+        {
+            if(distance(*pointIterator,(*clusterIterator).mean)<minimum)
+            {
+                minimum=distance(*pointIterator,(*clusterIterator).mean);
+                tempIterator=clusterIterator;
+            }
+        }
+        (*tempIterator).clusPoints.push_back(*pointIterator);
+    }
+
+    return clusters;
+}
+
+
+//bool isSameClustering (vector<cluster> pre, vector<cluster> now)
+//{
+//    vector<cluster>::iterator preItr;
+//    vector<cluster>::iterator nowItr;
+//
+//    for(nowItr=now.begin();nowItr!=now.end();nowItr++)
+//    {
+//        int marker = 0;
+//
+//        for(preItr=pre.begin();preItr!=pre.end();preItr++)
+//        {
+//            if(isSamePoint((*nowItr).mean,(*preItr).mean))
+//            {
+//                marker = 1;
+//                break;
+//            }
+//        }
+//
+//        if(marker==0)
+//            return false;
+//    }
+//
+//    return true;
+//}
+
+vector<cluster> kMeans (vector<point> points, int k)
+{
+    vector<cluster> clusters;
+    vector<cluster> temp1;
+    for(int i=0; i<k; i++)
+    {
+        cluster temp;
+        temp.mean=points[(points.size()/k)*i];
+        clusters.push_back(temp);
+    }
+
+    while(meanSameMarker!=k)
+    {
+        temp1=clustering(points,clusters);
+        clusters=meanPoints(temp1);
+    }
+    clusters=temp1;
+
+    return clusters;
 }
